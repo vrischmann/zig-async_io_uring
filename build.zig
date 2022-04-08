2c792b990139960417a1988bcae8bd27e26ee1bc
Vincent Rischmann <vincent@rischmann.fr>
2022-04-09 01:45:48 +0200

    update

--- build.zig
+++ build.zig
@@ -1,20 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("zig-async-io_uring", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.install();
+    exe.addPackagePath("async_io_uring", "third_party/async_io_uring/src/async_io_uring.zig");
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
@@ -24,11 +18,4 @@ pub fn build(b: *std.build.Builder) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&exe_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,97 @@
 const std = @import("std");
+const crypto = std.crypto;
+const fmt = std.fmt;
+const debug = std.debug;
+const os = std.os;
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+const IO_Uring = os.linux.IO_Uring;
+
+const AsyncIOUring = @import("async_io_uring").AsyncIOUring;
+
+const max_ring_entries = 512;
+
+fn computeFileHash(ring: *AsyncIOUring, path: [:0]const u8) ![crypto.hash.Sha1.digest_length]u8 {
+    const open_cqe = try ring.openat(
+        os.linux.AT.FDCWD,
+        path,
+        0,
+        0644,
+        null,
+        null,
+    );
+    const fd = @intCast(os.fd_t, open_cqe.res);
+
+    var statx_buf: os.linux.Statx = undefined;
+    _ = try ring.statx(
+        fd,
+        "",
+        os.linux.AT.EMPTY_PATH,
+        os.linux.STATX_SIZE,
+        &statx_buf,
+        null,
+        null,
+    );
+
+    // Read and compute hash
+
+    var hasher = crypto.hash.Sha1.init(.{});
+
+    var n: u64 = 0;
+    while (n < statx_buf.size) {
+        var buf: [4096]u8 = undefined;
+        _ = buf;
+
+        const cqe = try ring.read(
+            fd,
+            &buf,
+            n,
+            null,
+            null,
+        );
+
+        const read = @intCast(u64, cqe.res);
+        n += read;
+
+        hasher.update(buf[0..read]);
+    }
+
+    var hash: [crypto.hash.Sha1.digest_length]u8 = undefined;
+    hasher.final(&hash);
+
+    return hash;
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+fn runEventLoop(frame: *@Frame(computeFileHash), file_path: [:0]const u8) !void {
+    var ring = try IO_Uring.init(max_ring_entries, 0);
+    defer ring.deinit();
+
+    var async_ring = AsyncIOUring{ .ring = &ring };
+    try async_ring.run_event_loop();
+
+    frame.* = async computeFileHash(&async_ring, file_path);
+
+    return async_ring.run_event_loop();
+}
+
+pub fn main() anyerror!void {
+    const file_path = "/home/vincent/Music/Nemophila/NEMOPHILA-REVIVE/01-02-NEMOPHILA-DISSENSION-SMR.flac";
+
+    var file_hash_frame: @Frame(computeFileHash) = undefined;
+
+    var thread = try std.Thread.spawn(.{}, runEventLoop, .{
+        &file_hash_frame,
+        file_path,
+    });
+
+    debug.print("joining thread\n", .{});
+    thread.join();
+    debug.print("joined thread\n", .{});
+
+    //
+
+    const file_hash = try nosuspend await file_hash_frame;
+
+    debug.print("file hash: {s}\n", .{
+        fmt.fmtSliceHexLower(&file_hash),
+    });
 }
